18ebffb7cc9bc95c8020270ecf21d9b722eeeb12
xingtao <540850871@qq.com>
2019-11-21 15:24:02 +0800

    修改sh配置

--- sh/pm2.sh
+++ sh/pm2.sh
@@ -2,10 +2,10 @@
 
 #project_path=$(cd `dirname $0`; pwd)
 #project_name="${project_path##*/}"
-#root_path=$(dirname "$PWD")
+#parent_path=$(dirname "$PWD")
 #echo $project_path
 #echo $project_name
-#echo $root_path
+#echo $parent_path
 
 # system type
 sysOS=`uname -s`
@@ -13,18 +13,19 @@ sysOS=`uname -s`
 project_path=$(cd `dirname $0`; pwd)
 #echo $project_path
 # parent root
-root_path=$(dirname "$PWD")
+parent_path=$(dirname "$PWD")
+current_path="$PWD"
 
-if [[ ${sysOS} == "Darwin" && ${sysOS} == "Linux" ]];then
- config_path=${root_path}/ecosystem.config.js
+if [[ ${sysOS} == "Darwin" || ${sysOS} == "Linux" ]];then
+ config_path=${parent_path}/ecosystem.config.js
 else
- config_path=${root_path}/sh/ecosystem.config.js
+ config_path=${current_path}/ecosystem.config.js
 fi
 # start pm2 with config
 npx pm2 start ${config_path}
-
-# set logrotate limit 10 log files
+#
+## set logrotate limit 10 log files
 npx pm2 set pm2-logrotate:retain 10
 
 
-#exec /bin/bash
+exec /bin/bash
