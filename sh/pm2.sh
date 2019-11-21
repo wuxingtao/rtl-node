75dc4be4d0b0cc03b21e110d5c6c42daab89b6ec
Cloud User <centos@ip-172-31-20-133.ap-northeast-2.compute.internal>
2019-11-21 06:35:43 +0000

    add sh

--- sh/pm2.sh
+++ sh/pm2.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-#project_path=$(cd `dirname $0`; pwd)
-#project_name="${project_path##*/}"
-#root_path=$(dirname "$PWD")
-#echo $project_path
-#echo $project_name
-#echo $root_path
-
 root_path=$(dirname "$PWD")
 # start pm2 with config
 npx pm2 start $root_path/ecosystem.config.js
