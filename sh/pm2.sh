3c6529b650b6a4f39e7fa4656a555da13dbcfd86
xingtao <540850871@qq.com>
2019-11-21 14:43:31 +0800

    modify pm2.sh

--- sh/pm2.sh
+++ sh/pm2.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+#project_path=$(cd `dirname $0`; pwd)
+#project_name="${project_path##*/}"
+#root_path=$(dirname "$PWD")
+#echo $project_path
+#echo $project_name
+#echo $root_path
+
+project_path=$(cd `dirname $0`; pwd)
+# parent root
 root_path=$(dirname "$PWD")
 # start pm2 with config
-npx pm2 start $root_path/ecosystem.config.js
+npx pm2 start $project_path/ecosystem.config.js
 
 # set logrotate limit 10 log files
 npx pm2 set pm2-logrotate:retain 10
